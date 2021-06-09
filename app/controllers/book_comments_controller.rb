c8823bc3def3d0e898bfed76ab568143dcc45bff
MizukiSaitou <r2.f4g.gg9y.rj@gmail.com>
2021-06-09 06:22:14 +0000

    非同期通信

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,17 +1,18 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    book_comment = current_user.book_comments.new(book_comment_params)
-    book_comment.book_id = book.id
-    book_comment.save
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+    @user = @book.user
+    @book_comment = current_user.book_comments.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    @book_comment.save
   end
 
   def destroy
-    book_comment = BookComment.find_by(user_id: current_user.id, book_id: params[:book_id])
-    book_comment.destroy
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+    @user = @book.user
+    @book_comment = BookComment.find_by(user_id: current_user.id, book_id: params[:book_id])
+    @book_comment.destroy
   end
 
   private
@@ -20,4 +21,13 @@ class BookCommentsController < ApplicationController
     params.require(:book_comment).permit(:comment)
   end
 
+  def correct_user
+    @book_comment = current_user.comments.find_by(id: params[:id])
+    unless @book_comment
+    redirect_to request.referer
+    end
+  end
+
 end
+
+
